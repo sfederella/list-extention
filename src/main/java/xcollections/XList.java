03c2a42077197511b9a34147f8c18e5dcb87f8bc
Santiago Federella <santiago.federella@outlook.com>
2015-08-23 19:11:08 -0300

    agrego algunos metodos, falta seguir testeando

--- src/main/java/xcollections/XList.java
+++ src/main/java/xcollections/XList.java
@@ -1,12 +1,14 @@
 package xcollections;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class XList<E> extends ArrayList<E>{
 	
+	private Class<?> parameterClass;
 	private static final long serialVersionUID = 624622761768981865L;
 	
 	
@@ -33,24 +35,37 @@ public class XList<E> extends ArrayList<E>{
 	@SafeVarargs
 	public static <T>  XList<T> of(T ... args){
 		return new XList<T>(args);
+	}	
+	
+	public boolean add(E elem){
+		if(this.isEmpty()) setType(elem);
+		return super.add(elem);
 	}
-	
-	
-// Aca pongo algunos utils.
 	
 	@SuppressWarnings("unchecked")
-	public boolean add(E arg0, E ... args){
-		add(arg0);
+	public boolean add(E arg, E ... args){
+		if(this.isEmpty()) setType(arg);
+		boolean result = super.add(arg);
 		for(E elem : args){
-			add(elem);
+			result = result && super.add(elem);
 		}
-		return true;
+		return result;
 	}
+	
+	private void setType(E elem){
+		parameterClass = elem.getClass(); 
+	}
+	
+	public Class<?> getType(){
+		return parameterClass;
+	}
+
+// Aca pongo algunos utils.
 	
 	public XList<E> getInverse(){
 		XList<E> inverse = new XList<E>();
-		for(int i=this.size(); i>0; i--){
-			inverse.add(get(i));
+		for(int i=size(); i>0; i--){
+			inverse.add(get(i-1));
 		}
 		return inverse;
 	}
@@ -70,6 +85,41 @@ public class XList<E> extends ArrayList<E>{
 		return get(size()-1);
 	}
 	
+	/**
+	 * Si los elementos de la lista son numeros retorna su suma, caso contrario retorna null.
+	 * @author sfederella
+	 */
+	
+	@SuppressWarnings("unchecked")
+	public E sum(){
+		if(parameterClass==Integer.class) {
+			return (E) foldl(0,(e1,e2) -> (Integer)e1 + (Integer)e2);
+		} else if(parameterClass==Double.class) {
+			return (E) foldl(0.0,(e1,e2) -> (Double)e1 + (Double)e2);
+		} else {
+			return null; //XXX CHECK
+		}
+	}
+	
+	/**
+	 * Une la lista en un String separando los elementos por el "spliptter".
+	 * Usa el metodo ".toString()" por cada elemento. 
+	 * @author sfederella
+	 * @param splitter
+	 */
+	
+	public String join(String splitter){
+		String joined = "";
+		E firstElem = get(0);
+		for(E elem : this){
+			if(elem!=firstElem) {
+				joined = joined.concat(splitter);
+			}
+			joined = joined.concat(elem.toString());
+		}
+		return joined;
+	}
+	
 // Aca pongo algunas funciones de orden superior.
 	
 	public <T> XList<T> map(Function<? super E, ? extends T> mapper){
@@ -77,6 +127,15 @@ public class XList<E> extends ArrayList<E>{
 		for(E elem : this){
 			T mappedElem = mapper.apply(elem);
 			mappedList.add(mappedElem);
+		}
+		return mappedList;
+	}
+	
+	public <T> XList<T> flatMap(Function<? super E, ? extends List<? extends T>> mapper){
+		XList<T> mappedList = new XList<T>();
+		for(E elem : this){
+			List<? extends T> auxList = mapper.apply(elem);
+			mappedList.addAll(auxList);
 		}
 		return mappedList;
 	}
@@ -95,7 +154,7 @@ public class XList<E> extends ArrayList<E>{
 		T result = seed; 
 		for (E elem : this)
 		     result = reducer.apply(result, elem);
-		 return result;   
+		return result;   
 	}
 	
 	public <T> T foldr(T seed, BiFunction<T, ? super E, T> reducer){
--- src/test/java/xcollections/TestXList.java
+++ src/test/java/xcollections/TestXList.java
@@ -13,6 +13,11 @@ public class TestXList {
 	}
 	
 	@Test
+	public void testAVerSiAndaElGetInverse() {
+		assertEquals("[andas, como, hola]",xlist.getInverse().toString());
+	}
+	
+	@Test
 	public void testAVerSiAndaElMap() {
 		assertEquals("[4, 4, 5]",xlist.map(e -> e.length()).toString());
 	}
@@ -23,8 +28,13 @@ public class TestXList {
 	}
 	
 	@Test
+	public void testAVerSiAndaElFoldr() {
+		assertEquals((Integer)(-13),xlist.foldr(0,(e1,e2) -> e1-e2.length()));
+	}
+	
+	@Test
 	public void testAVerSiAndaElFoldl1() {
-		assertEquals((Integer)13,xlist.map(e -> e.length()).foldl1((e1,e2) -> e1+e2));
+		assertEquals((Integer)6,XList.of(1,2,3).foldl1((e1,e2) -> e1+e2));
 	}
 	
 	@Test
@@ -32,4 +42,32 @@ public class TestXList {
 		xlist.add("todo","bien");
 		assertEquals("[hola, como, andas, todo, bien]",xlist.toString());
 	}
+	
+	@Test
+	public void testAVerSiAndaElSumConInt() {
+		assertEquals((Integer)13,xlist.map(e -> e.length()).sum());
+	}
+	
+	@Test
+	public void testAVerSiAndaElSumConDouble() {
+		assertEquals((Double)7.0,XList.of(1.0,2.0,4.0).sum());
+	}
+	
+	@Test
+	public void testAVerSiAndaElSumConListaVacia() {
+		assertEquals(null,XList.of().sum());
+	}
+	
+	@Test
+	public void testAVerSiAndaElSumConOtro() {
+		assertEquals(null,xlist.sum());
+	}
+	
+	@Test
+	public void testAVerComoFuncionaElJoin() {
+		assertEquals("hola -> como -> andas",xlist.join(" -> "));
+		assertEquals("hola, como, andas",xlist.join(", "));
+		assertEquals("1+2+3",XList.of(1,2,3).join("+"));
+	}
+	
 }
